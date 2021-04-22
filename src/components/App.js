5144339910e98e35ef9b9c8209e5967cddd89ace
Nevaland <deanh0716@gmail.com>
2021-04-22 19:10:18 +0900

    Implement signup, login with email, google, github

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
-import { React, useState } from "react";
+import { React, useEffect, useState } from "react";
 import AppRouter from "components/Router";
 import { authService } from "fbInstance";
 
 function App() {
-  const [isLoggedIn, SetIsLoggedIn] = useState(authService.currentUser);
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, SetIsLoggedIn] = useState(false);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        SetIsLoggedIn(true);
+      } else {
+        SetIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn} />{" "}
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
       <footer>&copy; {new Date().getFullYear()} Twitter-Clone-Neva</footer>
     </>
   );
