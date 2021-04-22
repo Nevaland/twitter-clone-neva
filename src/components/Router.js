9efa3e810d2f65a60f499a930bb464acdcb8e743
Nevaland <deanh0716@gmail.com>
2021-04-22 19:21:06 +0900

    Add navigation

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,14 +2,28 @@ import { React } from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "components/Navigation";
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Switch>
-        <Route exact path="/">
-          {isLoggedIn ? <Home /> : <Auth />}
-        </Route>
+        {isLoggedIn ? (
+          <>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
+          </>
+        ) : (
+          <Route exact path="/">
+            <Auth />
+          </Route>
+        )}
       </Switch>
     </Router>
   );
